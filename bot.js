9e984048593c0c02fc659e662d3869526a22bd22
fathisye <47281515+fathisye@users.noreply.github.com>
2019-03-25 21:43:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,12 +6,12 @@ const prefix = "=";
 
 client.on('ready', () => {
    console.log(`----------------`);
-      console.log(`Desert Bot- Script By : EX Clan`);
+      console.log(`AboRoh Script By : AboRoh`);
         console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : AboRoh ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`=bc |AboRoh`,"http://twitch.tv/Death Shop")
+client.user.setGame(`=bc |AboRoh`,"http://twitch.tv/AboRoh")
 client.user.setStatus("dnd")
 });
 
